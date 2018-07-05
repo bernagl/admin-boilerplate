d7c0670786e9a2b853f0980039b153f60fc9bc05
bernagl <luisb.galo@gmail.com>
2018-07-04 20:48:14 -0500

    Fixed add button position on datatable

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -43,41 +43,52 @@ export default class Datatable extends Component {
     const { Columns, Inputs, model, submit, title } = this.props
     const action = selected ? updateDocument(model) : addDocument(model)
     return (
-      <div>
-        <Table
-          data={data}
-          columns={Columns(this.showModal)}
-          pagination={50}
-          searchPlaceholder="Buscar"
-          emptyText={() => 'Esta tabla aún no tiene ningún dato'}
-        />
-        <Modal
-          title={
-            title ? title : selected ? `Editar ${model}` : `Agregar ${model}`
-          }
-          visible={modal}
-          onOk={this.handleOk}
-          okText="Guardar"
-          onCancel={this.closeModal}
-          cancelText="Cancelar"
-          confirmLoading={loading}
-          destroyOnClose={true}
-        >
-          <Form
-            updateData={this.setDataToState}
-            action={action}
-            selected={selected}
-            submit={submit}
-            model={model}
-            closeModal={this.closeModal}
-            ref={this.formRef}
+      <div className="row">
+        <div className="col-12 my-2">
+          <Button
+            type="primary"
+            onClick={() => this.showModal()}
+            className="float-right"
           >
-            {Inputs(selected ? selected : {})}
-          </Form>
-        </Modal>
-        <Button type="primary" onClick={() => this.showModal()}>
-          Agregar
-        </Button>
+            Agregar
+          </Button>
+        </div>
+        <div className="col-12">
+          <Table
+            columns={Columns(this.showModal)}
+            data={data}
+            emptyText={() => 'Esta tabla aún no tiene ningún dato'}
+            footer={false}
+            header={false}
+            pagination={50}
+            search={false}
+            searchPlaceholder="Buscar"
+          />
+          <Modal
+            title={
+              title ? title : selected ? `Editar ${model}` : `Agregar ${model}`
+            }
+            visible={modal}
+            onOk={this.handleOk}
+            okText="Guardar"
+            onCancel={this.closeModal}
+            cancelText="Cancelar"
+            confirmLoading={loading}
+            destroyOnClose={true}
+          >
+            <Form
+              updateData={this.setDataToState}
+              action={action}
+              selected={selected}
+              submit={submit}
+              model={model}
+              closeModal={this.closeModal}
+              ref={this.formRef}
+            >
+              {Inputs(selected ? selected : {})}
+            </Form>
+          </Modal>
+        </div>
       </div>
     )
   }
